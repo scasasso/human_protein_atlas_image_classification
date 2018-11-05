0d950e6a4e7def4ea33b8b0aeed9188053bebeb4
Stefano Casasso <stecasasso@gmail.com>
2018-11-05 07:00:53 +0100

    add class names

--- utils/defines.py
+++ utils/defines.py
@@ -6,6 +6,7 @@ DATA_DIR = os.path.join(PROJ_DIR, 'input')
 TRAIN_DIR = os.path.join(DATA_DIR, 'train')
 TEST_DIR = os.path.join(DATA_DIR, 'test')
 SUB_DIR = os.path.join(PROJ_DIR, 'submissions')
+SPLIT_DIR = os.path.join(DATA_DIR, 'splits')
 
 # Files
 LABEL_FILE = os.path.join(DATA_DIR, 'train.csv')
@@ -13,3 +14,11 @@ SAMPLE_SUB_FILE = os.path.join(DATA_DIR, 'sample_submission.csv')
 
 # Miscellanea
 N_CLASSES = 28
+CLASSES = ['Nucleoplasm', 'Nuclear membrane', 'Nucleoli', 'Nucleoli fibrillar center',
+           'Nuclear speckles', 'Nuclear bodies', 'Endoplasmic reticulum', 'Golgi apparatus',
+           'Peroxisomes', 'Endosomes', 'Lysosomes', 'Intermediate filaments', 'Actin filaments',
+           'Focal adhesion sites', 'Microtubules', 'Microtubule ends', 'Cytokinetic bridge',
+           'Mitotic spindle', 'Microtubule organizing center', 'Centrosome', 'Lipid droplets',
+           'Plasma membrane', 'Cell junctions', 'Mitochondria', 'Aggresome', 'Cytosol',
+           'Cytoplasmic bodies', 'Rods & rings']
+CLAS_DICT = dict(zip(range(N_CLASSES), CLASSES))
