a6ee8c163e4d4502c24a8228ed02c6904788904d
Stefano Casasso <stecasasso@gmail.com>
2018-11-05 07:01:34 +0100

    edit f1 metric

--- utils/keras.py
+++ utils/keras.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import keras.backend as K
 
 
-def f1(y_true, y_pred, threshold=0.5):
+def f1_v0(y_true, y_pred, threshold=0.5):
     y_pred = K.cast(K.greater(K.clip(y_pred, 0, 1), threshold), K.floatx())
     tp = K.sum(K.cast(y_true * y_pred, 'float'), axis=0)
     # tn = K.sum(K.cast((1 - y_true) * (1 - y_pred), 'float'), axis=0)
@@ -17,8 +17,23 @@ def f1(y_true, y_pred, threshold=0.5):
     return K.mean(f1)
 
 
+def f1(y_true, y_pred):
+    # y_pred = K.cast(K.greater(K.clip(y_pred, 0, 1), threshold), K.floatx())
+    tp = K.sum(K.cast(y_true * y_pred, 'float'), axis=0)
+    tn = K.sum(K.cast((1 - y_true) * (1 - y_pred), 'float'), axis=0)
+    fp = K.sum(K.cast((1 - y_true) * y_pred, 'float'), axis=0)
+    fn = K.sum(K.cast(y_true * (1 - y_pred), 'float'), axis=0)
+
+    p = tp / (tp + fp + K.epsilon())
+    r = tp / (tp + fn + K.epsilon())
+
+    f1 = 2 * p * r / (p + r + K.epsilon())
+    f1 = tf.where(tf.is_nan(f1), tf.zeros_like(f1), f1)
+    return K.mean(f1)
+
+
 def f1_0p05(y_true, y_pred):
-    return f1(y_true, y_pred, 0.05)
+    return f1_v0(y_true, y_pred, 0.05)
 
 
 def f1_loss(y_true, y_pred):
